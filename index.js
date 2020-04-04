0e962fa933778b6ae0f3cb6ffdb2a6d4ecd81d5c
Kostya Gorodnichuk <coolkostya@gmail.com>
2020-04-05 02:21:23 +0300

    feat: add random filling of cells

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const n=3;
+const n=5;
 function gridTamplate(n){
     let str="";
     for (let i = 0; i < n; i++) {
@@ -6,29 +6,44 @@ function gridTamplate(n){
     }
     return str+"; ";
 } 
-console.log(gridTamplate(n))
 var field = document.createElement('div');
 field.setAttribute("class","field");
 field.setAttribute("style",
     "grid-template-columns: "+gridTamplate(n)+
     "grid-template-rows: "+gridTamplate(n)
 );
-
-for (let i = 1; i < n+1; i++) {
-    for (let k = 1; k < n+1; k++) {
+function randomNumbers(){
+    var arr=[];
+    var i2;
+    var sub;
+	for (let i = 0; i < Math.pow(n,2)-1; i++) {
+        arr.unshift(i+1);
+        i2=Math.floor(Math.random()*(i + 1));
+        sub = arr[i2];
+        arr[i2]=arr[0];
+        arr[0]=sub;
+    }
+	return arr;
+}
+console.log(randomNumbers());
+const arrOfNumb = randomNumbers()
+for (let i = 1; i <= n; i++) {
+    for (let k = 1; k <= n; k++) {
         const cell = document.createElement('div');
         const inCell = document.createElement('div');
         cell.setAttribute('class','cell');
         cell.setAttribute('id','c'+i+'-'+k);
         inCell.setAttribute('class','inCell');
+        inCell.innerText=k==n&&i==n?"":arrOfNumb[((i-1)*3+k)-1];
+        console.log((i-1)*3+k);
         field.append(cell);
         cell.append(inCell);
     }
 }
 document.body.append(field);
 //=====================Анимация
-// const field = document.getElementsByClassName('field')[0];
-// field.addEventListener('mousedown',(e)=>{
+
+// document.getElementsByClassName('field')[0].addEventListener('mousedown',(e)=>{
 //     e.target.classList.add('a');
 //     e.target.style = " transform: translateX(100%)"
 //     setTimeout(()=>{
