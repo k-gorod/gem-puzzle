7abaa287c4fa5a9ce31ec012abeb1870a79ca912
Kostya Gorodnichuk <coolkostya@gmail.com>
2020-04-06 19:22:04 +0300

    refactor: add Field class

--- index.js
+++ index.js
@@ -1,68 +1,79 @@
-const n=8;
-function gridTamplate(n){
-    let str="";
-    for (let i = 0; i < n; i++) {
-        str+="1fr "
-    }
-    return str+"; ";
-} 
-var field = document.createElement('div');
-field.setAttribute("class","field");
-field.setAttribute("style",
-    "grid-template-columns: "+gridTamplate(n)+
-    "grid-template-rows: "+gridTamplate(n)
-);
-function randomNumbers(){
-    var arr=[];
-    var i2;
-    var sub;
-	for (let i = 0; i < Math.pow(n,2)-1; i++) {
-        arr.unshift(i+1);
-        i2=Math.floor(Math.random()*(i + 1));
-        sub = arr[i2];
-        arr[i2]=arr[0];
-        arr[0]=sub;
-    }
-	return arr;
-}
 
-const arrOfNumb = randomNumbers()
-for (let i = 1; i <= n; i++) {
-    for (let k = 1; k <= n; k++) {
-        const cell = document.createElement('div');
-        const inCell = document.createElement('div');
-        cell.setAttribute('class','cell');
-        cell.setAttribute('id','c'+i+'-'+k);
-        inCell.setAttribute('class','inCell');
-        if(k==n&&i==n){
-            cell.classList.add('void');
-        }else{
-            inCell.innerText=arrOfNumb[((i-1)*3+k)-1];
-        }
-        field.append(cell);
-        cell.append(inCell);
-    }
+window.onload=()=>{
+    const gemField = new Field(4);
+    gemField.create();
+    moveCells();
 }
-document.body.append(field);
-let res = true;
-document.getElementsByClassName('field')[0].addEventListener('mousedown',(e)=>{
+function moveCells(){
+    let res = true;
+    document.getElementsByClassName('field')[0].addEventListener('mousedown',(e)=>{
     var cell=e.target.classList.contains('inCell')?e.target.parentElement:false;
     if(cell){
         if(res){
             res=false;
             setTimeout(()=>{res=true},200)
-            tryToMove(cell);
-        }//else{cell.shake()}
+            const targetCell = new Cell(cell);
+            targetCell.tryToMove();
+        }//else{targetCell.err()}
     }
 })
-function tryToMove(cell){
-    let targetCell = new Cell(cell);
-    if(Math.abs(targetCell.inColumn())==1){
-        targetCell.replace(targetCell.inColumn(),"Y");
+}
+class Field{
+    constructor(n){
+        this.n = n;
     }
-    if(Math.abs(targetCell.inRow())==1){
-        targetCell.replace(targetCell.inRow(),"X");
+    gridTamplate(n){
+        let str="";
+        for (let i = 0; i < n; i++) {
+            str+="1fr "
+        }
+        return str+"; ";
     }
+    randomNumbers(){
+        var arr=[];
+        var i2;
+        var sub;
+        for (let i = 0; i < Math.pow(this.n,2)-1; i++) {
+            arr.unshift(i+1);
+            i2=Math.floor(Math.random()*(i + 1));
+            sub = arr[i2];
+            arr[i2]=arr[0];
+            arr[0]=sub;
+        }
+        return arr;
+    }
+    fillUp(field,arrOfNumb){
+        for (let i = 1; i <= this.n; i++) {
+            for (let k = 1; k <= this.n; k++) {
+                
+                const cell = document.createElement('div');
+                const inCell = document.createElement('div');
+                cell.setAttribute('class','cell');
+                cell.setAttribute('id','c'+i+'-'+k);
+                inCell.setAttribute('class','inCell');
+                if(k==this.n&&i==this.n){
+                    cell.classList.add('void');
+                }else{
+                    inCell.innerText=arrOfNumb[((i-1)*3+k)-1];
+                }
+                field.append(cell);
+                cell.append(inCell);
+            }
+        }
+        
+        
+    }
+    create(){
+        const field = document.createElement('div');
+        field.setAttribute("class","field");
+        field.setAttribute("style",
+            "grid-template-columns: "+this.gridTamplate(this.n)+
+            "grid-template-rows: "+this.gridTamplate(this.n)
+        );
+        this.fillUp(field,this.randomNumbers())
+        document.body.append(field);
+    }
+
 }
 class Cell{
     constructor(cell){
@@ -81,6 +92,14 @@ class Cell{
                 this.voidCell.id[3]-this.row:
                 false
     }
+    tryToMove(){
+        if(Math.abs(this.inColumn())==1){
+            this.replace(this.inColumn(),"Y");
+        }
+        if(Math.abs(this.inRow())==1){
+            this.replace(this.inRow(),"X");
+        }
+    }
     replace(diff,direction){
         let id;
         let move=diff==1?(100):-100;
@@ -95,14 +114,4 @@ class Cell{
         },200)
     }
 }
-//=====================Анимация
 
-// document.getElementsByClassName('field')[0].addEventListener('mousedown',(e)=>{
-//     e.target.classList.add('a');
-//     e.target.style = " transform: translateX(100%)"
-//     setTimeout(()=>{
-//         e.target.classList.remove('a');
-//         e.target.style.transform = "none";
-//         e.target.style = "grid-area:1/2/2/3"
-//     },500)
-// });
