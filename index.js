41d46de55e5652fa187b1190569c0339410bc01e
Kostya Gorodnichuk <coolkostya@gmail.com>
2020-04-06 18:29:42 +0300

    feat: add algorithm of cell moving
    1. Add Cell class and moving funcs
    2. Add animation of moving
    3. Fix bugs

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const n=5;
+const n=8;
 function gridTamplate(n){
     let str="";
     for (let i = 0; i < n; i++) {
@@ -25,7 +25,7 @@ function randomNumbers(){
     }
 	return arr;
 }
-console.log(randomNumbers());
+
 const arrOfNumb = randomNumbers()
 for (let i = 1; i <= n; i++) {
     for (let k = 1; k <= n; k++) {
@@ -34,13 +34,67 @@ for (let i = 1; i <= n; i++) {
         cell.setAttribute('class','cell');
         cell.setAttribute('id','c'+i+'-'+k);
         inCell.setAttribute('class','inCell');
-        inCell.innerText=k==n&&i==n?"":arrOfNumb[((i-1)*3+k)-1];
-        console.log((i-1)*3+k);
+        if(k==n&&i==n){
+            cell.classList.add('void');
+        }else{
+            inCell.innerText=arrOfNumb[((i-1)*3+k)-1];
+        }
         field.append(cell);
         cell.append(inCell);
     }
 }
 document.body.append(field);
+let res = true;
+document.getElementsByClassName('field')[0].addEventListener('mousedown',(e)=>{
+    var cell=e.target.classList.contains('inCell')?e.target.parentElement:false;
+    if(cell){
+        if(res){
+            res=false;
+            setTimeout(()=>{res=true},200)
+            tryToMove(cell);
+        }//else{cell.shake()}
+    }
+})
+function tryToMove(cell){
+    let targetCell = new Cell(cell);
+    if(Math.abs(targetCell.inColumn())==1){
+        targetCell.replace(targetCell.inColumn(),"Y");
+    }
+    if(Math.abs(targetCell.inRow())==1){
+        targetCell.replace(targetCell.inRow(),"X");
+    }
+}
+class Cell{
+    constructor(cell){
+        this.cell = cell;
+        this.col = cell.id[1];
+        this.row = cell.id[3];
+        this.voidCell = document.getElementsByClassName('void')[0];
+    }
+    inRow(){
+        return  this.voidCell.id[3]==this.row?
+                this.voidCell.id[1]-this.col:
+                false
+    }
+    inColumn(){
+        return  this.voidCell.id[1]==this.col?
+                this.voidCell.id[3]-this.row:
+                false
+    }
+    replace(diff,direction){
+        let id;
+        let move=diff==1?(100):-100;
+        this.cell.classList.add('a');
+        this.cell.style = " transform: translate"+direction+"("+move+"%)"
+        setTimeout(()=>{
+            this.cell.classList.remove('a');
+            this.cell.style.transform = "none";
+            id=this.cell.id;
+            this.cell.id=this.voidCell.id;
+            this.voidCell.id=id;
+        },200)
+    }
+}
 //=====================Анимация
 
 // document.getElementsByClassName('field')[0].addEventListener('mousedown',(e)=>{
